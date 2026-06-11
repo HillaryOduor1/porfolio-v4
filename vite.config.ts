0c2b276d828fc85d6c1b5db13ef1b1de9e2f1946
Hillary <hillaryoduor0@gmail.com>
2026-06-11 15:53:59 +0300

    redirects to https://hillary-porfolio-v2.vercel.app/

--- vite.config.ts
+++ vite.config.ts
@@ -79,25 +79,32 @@ export default defineConfig({
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+//import legacy from 'vite-plugin-legacy-swc'
 
 export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ['ie >= 11', 'chrome >= 49', 'firefox >= 45', 'safari >= 10', 'edge >= 14'],
+      targets: ['ie >= 11', 'chrome >= 49', 'firefox >= 45', 'safari >= 10', 'edge >= 14','Android >= 44', 'iOS >= 10'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
       modernPolyfills: true,
       polyfills: [
         'es.promise',
         'es.object.assign',
         'es.array.iterator',
-        'es.string.iterator',
-        'web.dom.iterable'
+        'es.string.iterator'
       ]
     })
   ],
   build: {
     target: 'es5',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
     rollupOptions: {
       output: {
         manualChunks: undefined
